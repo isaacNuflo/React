5e86f5388732e0625db2140b0178ee6b2bfb6ac4
isaac.nuflo <isaac.nuflo@pandero.com.pe>
2021-04-22 14:29:04 -0500

    gifgriditem

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { GifGridItem } from './GifGridItem'
 
 const GifGrid = ({category}) => {
 
+    const [images, setImages] = useState([]) 
+    useEffect( () => {
+        getGifs(category)
+    }, [category])
+
     const apikey = 'IGKCgu17je9jvSR42vAIsLxNwGkEelG0';
 
-    const getGifs = () => {
-        
+    const getGifs = async (query) => {
+        const peticion = await fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=10&api_key=${apikey}`);
+        const {data} = await peticion.json();
+        const gifs = data.map( img => {
+            return {
+                id: img.id,
+                title: img.title,
+                url: img.images?.downsized_medium.url
+            }
+        })
+        setImages(gifs)
     }
 
     return (
         <>
             <h3>{category}</h3>
+                {images.map((img) => 
+                    <GifGridItem 
+                        key={img.id}
+                        {...img} 
+                    />
+                )}   
         </>
     )
 }
